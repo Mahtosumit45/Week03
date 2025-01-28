3c31baecae1d589b28c45ba5362e068a879e18ee
sumit kumar mahto <kumarmahtosumit45@gmail.com>
2025-01-28 22:52:12 +0530

    Singly link list

---
+++ Day01/src/SinglyLinkedList/SocialMediaFriendConnections/Friend.java
@@ -0,0 +1,13 @@
+package SinglyLinkedList.SocialMediaFriendConnections;
+
+import java.util.*;
+
+class Friend {
+    int friendId;
+    Friend next;
+
+    public Friend(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
