ae7cfbd116dc266a56e9b4b9afc3456916b7909d
sumit kumar mahto <kumarmahtosumit45@gmail.com>
2025-02-03 16:21:05 +0530

    [Sumit]add.Adding file reader proble

---
+++ Day04/src/FileReader/Problem1/FileReaderUtility.java
@@ -0,0 +1,19 @@
+package FileReader.Problem1;
+
+import java.io.*;
+
+class FileReaderUtility {
+    public static void readFile(String filePath) {
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
