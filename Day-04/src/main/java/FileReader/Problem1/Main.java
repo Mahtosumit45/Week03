14407589364ff02827c3958a638c3bd58860f85e
sumit kumar mahto <kumarmahtosumit45@gmail.com>
2025-02-07 15:01:05 +0530

    Delete Day-04 directory

--- Day-04/src/main/java/FileReader/Problem1/Main.java
+++
@@ -1,8 +0,0 @@
-package FileReader.Problem1;
-
-public class Main {
-    public static void main(String[] args) {
-        String filePath = "example.txt"; // Replace with your file path
-        FileReaderUtility.readFile(filePath);
-    }
-}
